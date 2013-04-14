be508e64e9a495e2622e9135a6ec10e2e2f8973f
Chris Kincanon <Catsith@gmail.com>
2013-04-14 13:11:59 -0700

    adding in fisrt workings of chat app

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
-var partials = require('express-partials');
 var app = express();
+var partials = require('express-partials');
+var fs = require('fs');
 
 app.engine('hamlc', require('haml-coffee').__express);
 app.use(partials());
+app.use(express.logger());
 
 app.configure(function() {
   app.set('view engine', 'hamlc');
@@ -14,5 +16,21 @@ app.get('/', function(req, res) {
   res.render('index', { name: 'Express user' });
 })
 
-app.listen(3000);
-console.log('App started on port 3000');
+var port = process.env.PORT || 5000;
+var server = require('http').createServer(app).listen(port);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(client){
+  client.on('join',function(name){
+    client.set('nickname',name)
+  });
+
+  client.on("message",function (data){
+    client.get('nickname',function(err,name){
+      var message = "<li class='chat-line'><span class='name'>"+name+"</span><span class='message'>"+data+"</span></li>";
+      client.broadcast.emit("messages", message);
+    });
+  });
+});
+
+console.log('App started on port '+port);
