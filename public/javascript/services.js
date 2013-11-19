ac9a17b332f7e7737f32ee18fc0fd8145d8d78cd
Chris Kincanon <Chris.Kincanon@gmail.com>
2013-11-19 00:08:23 -0800

    getting angular in place

---
+++ public/javascript/services.js
@@ -0,0 +1,25 @@
+var confluenceServices = angular.module('confluenceServices', []);
+
+confluenceServices.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {  
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
--- server.js
+++ server.js
@@ -21,6 +21,10 @@ var io = require('socket.io').listen(server);
 
 console.log('http server listening on %d', port);
 
+app.get('/messages', function(req,res){
+  res.json([{body: 'hello'},{body: 'world'}]);
+});
+
 app.get('/', function(req, res) {
   res.render('index', { name: 'Express user' });
 })
@@ -28,23 +32,103 @@ app.get('/', function(req, res) {
 io.sockets.on('connection', function (socket) {
   console.log('websocket connection open');
 
-  socket.on('join',function(name){
-    socket.set('nickname',name)
+  var name = userNames.getGuestName();
+  // send the new user their name and a list of users
+  socket.emit('init', {
+    name: name,
+    users: userNames.get()
   });
 
-  socket.on("message",function (data){
-    socket.get('nickname',function(err,name){
-      var message = "<li class='chat-line'><span class='name'>"+name+": </span><span class='message'>"+data+"</span></li>";
-      socket.broadcast.emit("messages", message);
+  // notify other clients that a new user has joined
+  socket.broadcast.emit('user:join', {
+    name: name
+  });
+
+  // broadcast a user's message to other users
+  socket.on('send:message', function (data) {
+    socket.broadcast.emit('send:message', {
+      user: name,
+      text: data.message
     });
   });
 
-  socket.on('disconnect', function() {
-    console.log('websocket connection close');
+  // validate a user's name change, and broadcast it on success
+  socket.on('change:name', function (data, fn) {
+    if (userNames.claim(data.name)) {
+      var oldName = name;
+      userNames.free(oldName);
+
+      name = data.name;
+
+      socket.broadcast.emit('change:name', {
+        oldName: oldName,
+        newName: name
+      });
+
+      fn(true);
+    } else {
+      fn(false);
+    }
+  });
+
+  // clean up when a user leaves, and broadcast it to other users
+  socket.on('disconnect', function () {
+    socket.broadcast.emit('user:left', {
+      name: name
+    });
+    userNames.free(name);
   });
 });
 
 
+var userNames = (function () {
+  var names = {};
+
+  var claim = function (name) {
+    if (!name || userNames[name]) {
+      return false;
+    } else {
+      userNames[name] = true;
+      return true;
+    }
+  };
+
+  // find the lowest unused "guest" name and claim it
+  var getGuestName = function () {
+    var name,
+      nextUserId = 1;
+
+    do {
+      name = 'Guest ' + nextUserId;
+      nextUserId += 1;
+    } while (!claim(name));
+
+    return name;
+  };
+
+  // serialize claimed names as an array
+  var get = function () {
+    var res = [];
+    for (user in userNames) {
+      res.push(user);
+    }
+
+    return res;
+  };
+
+  var free = function (name) {
+    if (userNames[name]) {
+      delete userNames[name];
+    }
+  };
+
+  return {
+    claim: claim,
+    free: free,
+    get: get,
+    getGuestName: getGuestName
+  };
+}());
 
 
 
